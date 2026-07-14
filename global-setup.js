5de114631bed011cbf9b70826021a9a02b5ef1a9
Seletria <esra.kocaoglu.contact@gmail.com>
2026-07-14 15:51:31 +0300

    chore: add global setup configuration

---
+++ global-setup.js
@@ -0,0 +1,20 @@
+// global-setup.js
+import { chromium } from '@playwright/test';
+import { LoginPage } from './pages/LoginPage';
+
+async function globalSetup(config) {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login('Admin', 'admin123');
+
+  await page.waitForURL(/.*dashboard.*/);
+
+  await page.context().storageState({ path: 'storageState.json' });
+
+  await browser.close();
+}
+
+export default globalSetup;
